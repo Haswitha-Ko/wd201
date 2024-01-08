36228ebef73bc2687008783c13ed1fdbed2bd0be
Haswitha-Ko <119152181+Haswitha-Ko@users.noreply.github.com>
2024-01-08 09:57:06 +0530

    Create app.js

---
+++ todo-app/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
+
+app.delete("/todos/:id", async function (request, response) {
+  try {
+    const deletedTodoCount = await Todo.destroy({
+      where: { id: request.params.id }, // Specify the Todo to delete by ID
+    });
+
+    // Respond based on whether the Todo was deleted or not
+    if (deletedTodoCount > 0) {
+      return response.send(true); // Todo was deleted
+    } else {
+      return response.send(false); // Todo with the given ID was not found
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
+});
+
+module.exports = app;
